221c55cbce7943d8fe19a760ef465c2e4b33d8d4
littyofficials@gmail.com <BryteLitty>
2022-10-10 07:31:09 -0700

    add functionalities to mobile view

---
+++ index.js
@@ -0,0 +1,12 @@
+const openBtn = document.querySelector(".nav-icon");
+const closeBtn = document.querySelector(".close-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const navLinks = document.querySelectorAll(".list-item");
+
+openBtn.addEventListener("click", function() {
+    mobileMenu.classList.add("show-mobile-menu");
+});
+
+closeBtn.addEventListener("click", function() {
+    mobileMenu.classList.remove("show-mobile-menu");
+})
