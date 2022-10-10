3a9d54131b17b9d003c227d132de8a000e658c18
munyite001 <emunyite@gmail.com>
2022-10-10 17:49:02 +0300

    add link functionality to Mobile Menu

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const navLinks = document.querySelectorAll(".list-item");
 
 openBtn.addEventListener("click", function() {
-    mobileMenu.classList.add("show-mobile-menu");
+  mobileMenu.classList.add("show-mobile-menu");
 });
 
 closeBtn.addEventListener("click", function() {
+  mobileMenu.classList.remove("show-mobile-menu");
+});
+
+navLinks.forEach(function(link){
+  link.addEventListener('click', function(){
     mobileMenu.classList.remove("show-mobile-menu");
-})
+  });
+});
