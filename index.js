f295e3c1eb10f459f11764e43875b2fa790d5fae
littyofficials@gmail.com <BryteLitty>
2022-10-10 11:24:42 -0700

    fix linter error

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const openBtn = document.querySelector(".nav-icon");
-const closeBtn = document.querySelector(".close-btn");
-const mobileMenu = document.querySelector(".mobile-menu");
-const navLinks = document.querySelectorAll(".list-item");
+const openBtn = document.querySelector('.nav-icon');
+const closeBtn = document.querySelector('.close-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const navLinks = document.querySelectorAll('.list-item');
 
-openBtn.addEventListener("click", function() {
-  mobileMenu.classList.add("show-mobile-menu");
+openBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('show-mobile-menu');
 });
 
-closeBtn.addEventListener("click", function() {
-  mobileMenu.classList.remove("show-mobile-menu");
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('show-mobile-menu');
 });
 
-navLinks.forEach(function(link){
-  link.addEventListener('click', function(){
-    mobileMenu.classList.remove("show-mobile-menu");
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('show-mobile-menu');
   });
 });
